f09a1524fa6e1407fe840d928c311876acc69af7
Vivek Verma <bookyourown.mutual@gmail.com>
2022-09-22 00:18:17 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Jeweller BillBook - Invoice',
+      title: 'Jeweller BillBook',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
