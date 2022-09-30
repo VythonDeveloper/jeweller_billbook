03e84e09c6877737665ebc41a14748d4efb47ab1
Vivek Verma <bookyourown.mutual@gmail.com>
2022-09-30 10:13:49 +0530

    mort

--- lib/Home/dashboard.dart
+++ lib/Home/dashboard.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:jeweller_stockbook/Home/mortgage.dart';
 import 'package:jeweller_stockbook/Helper/user.dart';
 import 'package:jeweller_stockbook/Stock/lowStock.dart';
 import 'package:jeweller_stockbook/colors.dart';
